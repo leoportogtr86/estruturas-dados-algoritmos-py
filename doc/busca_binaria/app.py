a2891a0e3b17283ebf917b586bb21f6b78e55d3e
Leo Porto <leonardoreisporto@gmail.com>
2024-07-03 08:06:14 -0300

    busca binaria

---
+++ doc/busca_binaria/app.py
@@ -0,0 +1,17 @@
+def busca_binaria(lista, alvo):
+    inicio = 0
+    fim = len(lista) - 1
+
+    while inicio <= fim:
+        meio = (inicio + fim) // 2
+        if lista[meio] == alvo:
+            return meio
+        elif lista[meio] < alvo:
+            inicio = meio + 1
+        else:
+            fim = meio - 1
+
+    return -1
+
+
+print(busca_binaria([10, 20, 30, 40, 50], 30))
